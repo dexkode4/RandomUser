614d82879f1ac32f4b3b65e9c134cada3762d6eb
dexkode4 <dexkode4@gmail.com>
2021-01-14 09:39:49 +0100

    create a test folder

--- src/__tests__/App.test.tsx
+++ src/__tests__/App.test.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { render, screen, waitFor } from '@testing-library/react';
-import App from './App';
+import App from '../App';
 
 test('renders welcome message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/Welcome to your dashboard, kindl sort through the user base/i);
+  const linkElement = screen.getByText(/Welcome to your dashboard, kindly sort through the user base/i);
   expect(linkElement).toBeInTheDocument();
 });
 
@@ -12,12 +12,4 @@ test('renders three categories of button', () => {
   render(<App />);
   const buttons = screen.getAllByRole('button');
   expect(buttons.length).toBe(3)
-});
-
-it('renders five summarycards', async () => {
-  render(<App />);
-  await waitFor(() => {
-    const summarycards = screen.getAllByTestId('summary-card')
-    expect(summarycards.length).toBe(5)
-  });
 });
