1d25eacbdaf1a68c604ba72c2f7e7b0ae58be0ac
dexkode4 <dexkode4@gmail.com>
2021-01-14 14:41:51 +0100

    make dropdown list close onclicking outside

--- src/components/Dropdown/index.tsx
+++ src/components/Dropdown/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useContext } from 'react';
+import React, { useState, useRef, useContext, useEffect } from 'react';
 import styles from './Dropdown.module.scss';
 import { IoMdArrowDropdown } from 'react-icons/io'
 import { UserContext } from '../../context/userContext';
@@ -7,10 +7,25 @@ import { IData } from '../../context/userContext'
 function Dropdown() {
     const [isOpen, setIsOpen] = useState(false);
     const state = useContext(UserContext);
+    const dropDownRef = useRef<HTMLDivElement>(null);
+
+
+    useEffect(() => {
+        function handleClickOutside(event: any) {
+          if (dropDownRef.current && !dropDownRef.current.contains(event.target)) {
+            setIsOpen(false);
+          }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+        return () => {
+          document.removeEventListener('mousedown', handleClickOutside);
+        };
+      }, [dropDownRef, setIsOpen]);
+    
 
     return (
         <>
-            <div className={styles.dropdown} onClick={() => setIsOpen(!isOpen)}>
+            <div className={styles.dropdown} onClick={() => setIsOpen(!isOpen)} ref={dropDownRef}>
                 country <IoMdArrowDropdown />
                 {isOpen && <div className={styles.dropdownOptions}>
                     <ul>
